ca8a8902b622c99308662a322fc773f007800396
savi_wso2 <mysticsavy@gmail.com>
2017-01-18 14:37:44 +0530

    Removing digital display

--- features/device-types-feature/digital-display-plugin-feature/org.wso2.carbon.device.mgt.iot.digitaldisplay.feature/src/main/resources/dbscripts/h2.sql
+++
@@ -1,8 +0,0 @@
-
--- -----------------------------------------------------
--- Table `digitaldisplay_DEVICE`
--- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `digitaldisplay_DEVICE` (
-  `digitaldisplay_DEVICE_ID` VARCHAR(45) NOT NULL ,
-  `DEVICE_NAME` VARCHAR(100) NULL DEFAULT NULL,
-  PRIMARY KEY (`digitaldisplay_DEVICE_ID`) );
